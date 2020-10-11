eae7632159e175348ee7d54431eb679b10652f65
yu <arashi.arata.200@gmail.com>
2020-10-11 09:35:53 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
         end
     end
     
+    def counts(user)
+        @count_microposts = user.microposts.count
+    end
+    
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,11 @@ class MicropostsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
+  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = 'メッセージを投稿しました。'
+      flash[:success] = "メッセージを投稿しました。"
       redirect_to root_url
     else
       @microposts = current_user.microposts.order(id: :desc).page(params[:page])
@@ -16,20 +17,22 @@ class MicropostsController < ApplicationController
 
   def destroy
     @micropost.destroy
-    flash[:success] = 'メッセージを削除しました。'
+    flash[:success] = "メッセージを削除しました。"
     redirect_back(fallback_location: root_path)
   end
-
+  
   private
-
+  
+  
   def micropost_params
     params.require(:micropost).permit(:content)
   end
-
+  
   def correct_user
     @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
       redirect_to root_url
     end
   end
+  
 end
